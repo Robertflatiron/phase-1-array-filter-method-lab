4ca027d45747ab1671bdb87b03e8dd4ac7ffc932
Robert Gaspar <gaspar.robert7@gmail.com>
2021-11-12 00:17:16 -0600

    commit tree 246785adaf97dfd4aa52857e99d93b53a2b0fe60

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+// const allDrivers = drivers.filter((drivers) => {
+//     if (drivers === 'Bobby') {
+//         return drivers
+//     }
+// })
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function fuzzyMatch(array, searchTerm) {
+    return array.filter(function(arrayElement) {
+        return arrayElement.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0
+    })
+}
